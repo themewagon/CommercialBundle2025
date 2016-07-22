e6c06fbaca05f1896cae322baa831f6d9573a73b
Ashraful Prium <prium47@gmail.com>
2016-07-22 14:42:42 +0600

    Nav Bar Layout Onepage
    close #34

--- core/assets/js/core.js
+++ core/assets/js/core.js
@@ -25,8 +25,40 @@ function initSkrollr(){
     });
 }
 
+
+// Initilize Menuzord
+function initMenuzord(){
+	$("#menuzord").menuzord();
+}
+
+
+//On page scroll for #id targets
+function onPageScroll(){
+
+	$('a[href*=\\#]:not([href=\\#])').click(function() {
+
+		if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+		    var target = $(this.hash);
+		    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+		    if (target.length) {
+		        $('html,body').animate({
+		            scrollTop: target.offset().top
+		        }, 400, 'swing', function(){
+		          
+		        });
+		        return false;
+		    }
+		}
+	});
+}
+
+
+
+// Calling functions above after document ready
 $(document).ready(function($){
 
 	initSkrollr();
+	initMenuzord();
+	onPageScroll();
 
 });
