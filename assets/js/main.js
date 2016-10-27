0db780b880925d6a79074cd3643d2f3b89646c80
Ashraful Prium <prium47@gmail.com>
2016-10-27 16:52:20 +0600

    logo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,5 @@
 console.log('%c Proudly Crafted with ZiOn.', 'background: #222; color: #bada55');
+
+// @if ZION_ENV = 'DEBUG'
+//alert("debug mode!");
+// @endif 
--- product/HTML/assets/js/main.js
+++ product/HTML/assets/js/main.js
@@ -1,1 +1,2 @@
 console.log('%c Proudly Crafted with ZiOn.', 'background: #222; color: #bada55');
+
