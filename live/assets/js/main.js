841c2837a32807ac79403b6c15fbcf614af257ec
Sajal Das <87944544+Sajal13@users.noreply.github.com>
2025-02-11 10:46:47 +0600

    Delete live directory

--- live/assets/js/main.js
+++
@@ -1,1 +0,0 @@
-$.holdReady(!0),$("body").imagesLoaded({background:".background-holder"},function(){$("#preloader").removeClass("loading"),$.holdReady(!1),setTimeout(function(){$("#preloader").remove()},800)}),$(window).on("load",function(){$("*[data-inertia]").each(function(){$(this).inertia()})});
